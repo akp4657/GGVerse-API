160b5591317aed5dae9880c1d57bade1954f58ec
Spine <igniteboost.net@gmail.com>
2025-07-07 22:22:28 -0400

    Prisma and Supabase set up

---
+++ app.js
@@ -0,0 +1,28 @@
+// index.js
+const express = require('express');
+require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.test_User_Table.findMany(); // Model name matches Prisma schema
+    res.status(200).send(users)
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
